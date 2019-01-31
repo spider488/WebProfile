2872869b30c454245a04ba040ca26798547e7102
spider488 <th3425xogm@gmail.com>
2019-01-31 20:23:00 +0900

    initial setting

---
+++ src/components/painting.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Painting extends Component {
+    render() {
+        return (
+            <div id="media">
+                <p></p>
+            </div>
+        )
+    }
+}
+
+export default Painting;
